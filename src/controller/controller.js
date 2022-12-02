3bb652eec9a7c1ad0d88518f67b79b3c59b7add5
functionup-am <akshaymakwana350@gmail.com>
2022-12-03 00:13:39 +0530

    demo

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -5,7 +5,8 @@ const field = require('../model/field')
 const organizations = require('../model/organizations')
 const property = require('../model/property')
 const region = require('../model/region')
-
+const mongoose = require('mongoose')
+const { populate } = require('../model/crop')
 const createOrganizations=async function(req,res){ 
     try{
         let data=req.body
@@ -78,13 +79,12 @@ const createOrganizations=async function(req,res){
 const getOrganizationData = async function (req, res) {
     try {
       let conditions = req.query; 
-      // Checks whether studentId isa valid ObjectId
+      // Checks whether or organizationId is a valid ObjectId
         if(conditions.organizationId) {
           if (!mongoose.isValidObjectId(conditions.organizationId))return res.status(400).send({ status: false, msg: "Please Enter organizationId as a valid ObjectId" })}
   
-      // Fetching the Students  
-      let organization = await organizations.find({$and: [conditions]});
-  
+      // Fetching the organizations  
+      let organization = await organizations.find(conditions);
       if (organization.length == 0)return res.status(404).send({ status: false, msg: "No organizations found" });
   
       res.status(200).send({ status: true, data: organization });   
@@ -94,22 +94,90 @@ const getOrganizationData = async function (req, res) {
     }
   };
 
+
+  const getDataByCrop = async function (req, res) {  
+      try {
+        let conditions = req.query; 
+        // Checks whether fieldId is a valid ObjectId
+          if(conditions.fieldId) {
+            if (!mongoose.isValidObjectId(conditions.fieldId))return res.status(400).send({ status: false, msg: "Please Enter fieldId as a valid ObjectId" })}
+    
+        // Fetching the Students  
+        let crops = await cropCycleField.find(conditions).populate("cropId").populate("fieldId")         
+        if (crops.length == 0)return res.status(404).send({ status: false, msg: "No crops found" }); 
+    
+        res.status(200).send({ status: true, data: crops });   
+    
+      } catch (error) {
+        res.status(500).send({ status: false, msg: error.message });
+      }
+    };
   
+
 
   const getDataByField = async function (req, res) {
     try {
-      let conditions = req.query; 
-      // Checks whether studentId isa valid ObjectId
-        if(conditions.fieldId) {
-          if (!mongoose.isValidObjectId(conditions.fieldId))return res.status(400).send({ status: false, msg: "Please Enter fieldId as a valid ObjectId" })}
+      let conditions = req.query
+      if(conditions.organizationId) {
+        if (!mongoose.isValidObjectId(conditions.organizationId))return res.status(400).send({ status: false, msg: "Please Enter organizationId as a valid ObjectId" })}
+        // fetching the data...
+        let checkOrg = await organizations.findOne({_id: conditions.organizationId}); 
+        if(!checkOrg) return res.status(400).send({ status: false, msg: "organizationId is not present in database" });
+
+        let field1= await field.find().populate({
+          path    : 'regionId',
+          populate: [
+              { path: 'propertyId' }
+          ]
+     });
+     
+        let field2 = field1.filter((x)=>{
+                let c = x.regionId.propertyId.organizationId.toString()
+               
+                if(c===conditions.organizationId){
+                    return x 
+                }
+        });    
+      
+         if (field2.length == 0)return res.status(404).send({ status: false, msg: "No fields found" });
+        res.status(200).send({ status: true, data1: field2 });   
   
-      // Fetching the Students  
-      let fields = await field.find({$and: [conditions]});
-            
-      if (fields.length == 0)return res.status(404).send({ status: false, msg: "No fields found" });
-  
-      res.status(200).send({ status: true, data: fields });   
-  
+    } catch (error) {
+      console.log(error)
+        res.status(500).send({ status: false, msg: error.message });     
+    }
+  };  
+
+  const getDataByCropCylce = async function (req, res) {
+    try {
+      let conditions = req.query
+      if(conditions.propertyId) {
+        if (!mongoose.isValidObjectId(conditions.propertyId))return res.status(400).send({ status: false, msg: "Please Enter propertyId as a valid ObjectId" })}
+        // fetching the data...
+        let checkOrg = await property.findOne({_id: conditions.propertyId}); 
+        if(!checkOrg) return res.status(400).send({ status: false, msg: "propertyId is not present in database" });
+
+        let field1= await cropCycleField.find().populate({
+         
+          path    : 'fieldId',
+          populate: [
+              { path: 'regionId' }
+          ]
+         
+     }).populate({
+       path: 'cropId',        
+     })
+     let field2 = field1.filter((x)=>{
+      let c = x.fieldId.regionId.propertyId.toString()
+     
+      if(c===conditions.propertyId){
+          return x 
+      }
+});    
+
+if (field2.length == 0)return res.status(404).send({ status: false, msg: "No cropcycle founds" });
+res.status(200).send({ status: true, data1: field2 });   
+
     } catch (error) {
       res.status(500).send({ status: false, msg: error.message });
     }
@@ -117,4 +185,6 @@ const getOrganizationData = async function (req, res) {
 
 
 
- module.exports = {createOrganizations,createProperty,createRegion, createField,createCrop,createCropCycleProperty,createCropCycleField, getOrganizationData,getDataByField}
+
+
+ module.exports = {createOrganizations,createProperty,createRegion, createField,createCrop,createCropCycleProperty,createCropCycleField, getOrganizationData,getDataByCrop,getDataByField,getDataByCropCylce}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();   
-const {createOrganizations,createProperty,createRegion, createField,createCrop,createCropCycleProperty,createCropCycleField,getOrganizationData,getDataByField} = require('../controller/controller');
+const {createOrganizations,createProperty,createRegion, createField,createCrop,createCropCycleProperty,createCropCycleField,getOrganizationData,getDataByCrop,getDataByField,getDataByCropCylce} = require('../controller/controller');
 const { createUser,loginUser } = require('../controller/userController');
+const {authentication} = require('../middleware/auth')
 router.post('/organization/create', createOrganizations)
 router.post('/property/create', createProperty)
 router.post('/region/create', createRegion)
@@ -9,8 +10,11 @@ router.post('/field/create', createField)
 router.post('/crop/create', createCrop)
 router.post('/cropCycleProperty/create', createCropCycleProperty)
 router.post('/cropCycleField/create', createCropCycleField)
-router.get('/getOrganizationsData',getOrganizationData)
-router.get('/getDataByField',getDataByField)
+router.get('/getOrganizationsData',authentication,getOrganizationData)
+router.get('/getDataByCrop',authentication,getDataByCrop)
+router.get('/getDataByField',authentication,getDataByField)
+router.get('/getDataByCropCylce',authentication,getDataByCropCylce)
+
 
 router.post('/createUser',createUser)
 router.post('/loginUser',loginUser)
